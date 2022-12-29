761a9bb38e2955e80b5ada189ac10871d8e1e944
hiren maniya <hirenmaniya9824@gmail.com>
2022-12-29 17:10:34 +0530

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,10 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'View_data.dart';
+
+
+
+
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   // If you're going to use other Firebase services in the background, such as Firestore,
   // make sure you call `initializeApp` before using other Firebase services.
