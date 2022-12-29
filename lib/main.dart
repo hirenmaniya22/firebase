6d50ae58910ab62e3aa9a19095a31019ab1704e1
hiren maniya <hirenmaniya9824@gmail.com>
2022-12-29 17:13:13 +0530

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,7 @@ import 'View_data.dart';
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   // If you're going to use other Firebase services in the background, such as Firestore,
   // make sure you call `initializeApp` before using other Firebase services.
-  await Firebase.initializeApp();
-
+  await Firebase.initializeApp();print("hello");
   print("Handling a background message: ${message.messageId}");
 }
 void main() async {
